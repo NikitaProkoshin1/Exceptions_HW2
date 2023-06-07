3e804fd12da8918c0029bdc10f9e2d8367799567
NikitaProkoshin1 <115005189+NikitaProkoshin1@users.noreply.github.com>
2023-06-07 21:55:51 +0700

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,5 +1,5 @@
 // Если необходимо, исправьте данный код (задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
-package HomeWork2;
+package Exceptions_HW2;
 
 // Дан следующий код, исправьте его там, где требуется (задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
 public class Task2 {
